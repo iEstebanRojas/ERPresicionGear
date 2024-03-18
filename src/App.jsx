c35026d641ba92a33de9c47085dc0944d99f21f4
Esteban Rojas <estebanrojas9314@gmail.com>
2024-03-17 22:30:20 -0300

    Primer proyecto E-Commerse

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { ListContainer } from "./components/ListContainer";
+import { NavBar } from "./components/NavBar";
+
+function App() {
+  return (
+  <>
+  <NavBar />
+  <ListContainer greetings="hi"/> 
+  </>
+  )
+}
+
+export default App;
